0bf4ea0827d613ef3177857d5e8a0c4a5f393a59
mutaqim96 <mutaqim.ah@theiac.com>
2020-12-28 10:55:11 +0800

    Add Mailable Class.

---
+++ app/Mail/TrainingCreated.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class TrainingCreated extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('email.training-mailable')
+                    ->subject('Training Created using Mailable Class');
+    }
+}
---
+++ resources/views/email/training-mailable.blade.php
@@ -0,0 +1,9 @@
+<h1>Congratulations </h1>
+
+<p>YOur Training Created using mailable class</p>
+
+<p>
+  Training title: {{$title}}
+  <br>
+  Description : {{$description}}
+</p>
