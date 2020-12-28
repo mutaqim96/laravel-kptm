6b52769b29defed0d6a792f44b54bc0bad2b15ad
mutaqim96 <mutaqim.ah@theiac.com>
2020-12-28 11:13:25 +0800

    error handle. silap eja

--- app/Mail/TrainingCreated.php
+++ app/Mail/TrainingCreated.php
@@ -6,7 +6,7 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
-use App\Model\Training;
+use App\Models\Training;
 
 class TrainingCreated extends Mailable
 {
