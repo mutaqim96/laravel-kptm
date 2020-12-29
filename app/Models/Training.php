5c2d78ad7e55421a563b1a52a7363db45f650995
mutaqim96 <mutaqim.ah@theiac.com>
2020-12-29 12:34:13 +0800

    Cara lain nak panggil semua ni.

--- app/Models/Training.php
+++ app/Models/Training.php
@@ -5,8 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Owenit\Auditing\Contracts\Auditable;
 
-class Training extends Model implements Owenit\Auditing\Contracts\Auditable //kita guna auditable interface.
+class Training extends Model implements Auditable //kita guna auditable interface.
 {
     use HasFactory;
     use SoftDeletes;
