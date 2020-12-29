4e2a62dbf5f3c3e6c507b12593db506d20373135
mutaqim96 <mutaqim.ah@theiac.com>
2020-12-29 12:33:04 +0800

    Add Audit Trail

--- app/Models/Training.php
+++ app/Models/Training.php
@@ -6,10 +6,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Training extends Model
+class Training extends Model implements Owenit\Auditing\Contracts\Auditable //kita guna auditable interface.
 {
     use HasFactory;
     use SoftDeletes;
+    use OwenIt\Auditing\Auditable; //kita guna auditable trait
 
     protected $fillable = ['title','description','trainer','attachment'];
 
